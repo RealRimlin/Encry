b72028401d0541e2c2952f1c133b16f0bb3f4e31
Ilya Oskin <ilya.arcadich@gmail.com>
2017-12-08 10:04:01 +0300

    NodeViewHolder implementation added.

--- src/main/scala/encry/nodeView/SimpleNodeViewHolder.scala
+++ src/main/scala/encry/nodeView/SimpleNodeViewHolder.scala
@@ -1,7 +1,9 @@
 package encry.nodeView
 
+import encry.modifiers.mempool.BasicMempool
 import encry.{SimpleBlockchain, SimpleSyncInfo}
-import encry.transaction.{EncryBaseTransaction, EncryMinimalState, SimpleBlock}
+import encry.transaction.{EncryBaseTransaction, EncryMinimalState, SimpleBlock, SimpleBlockCompanion}
+import scorex.core.serialization.Serializer
 import scorex.core.settings.{NetworkSettings, ScorexSettings}
 import scorex.core.transaction.box.proposition.PublicKey25519Proposition
 import scorex.core.{ModifierId, ModifierTypeId, NodeViewHolder, NodeViewModifier}
@@ -14,6 +16,14 @@ class SimpleNodeViewHolder[StateType <: EncryMinimalState[StateType]](settings: 
   override type SI = SimpleSyncInfo
   override type HIS = SimpleBlockchain
   override type MS = StateType
-  override type MP = this.type
+  override type MP = BasicMempool
+  override type VL = this.type
+
+  override lazy val modifierSerializers: Map[ModifierTypeId, Serializer[_ <: NodeViewModifier]] =
+    Map(SimpleBlock.ModifierTypeId -> SimpleBlockCompanion)
+
+  override def restoreState(): Option[(HIS, MS, VL, MP)] = None
+
+  override protected def genesisState: (HIS, MS, VL, MP) = ???
 
 }
